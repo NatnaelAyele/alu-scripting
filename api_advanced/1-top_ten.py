7962848635b87346105674479aaa7dec36c1f0c0
NatnaelAyele <n.eticha@alustudent.com>
2026-02-18 18:53:15 +0300

    solution to q1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -4,7 +4,6 @@ import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts for a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
 
     headers = {

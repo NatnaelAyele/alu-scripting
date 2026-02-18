a24249df61d3ee5aff5b4e18635421228e3e7ee5
NatnaelAyele <n.eticha@alustudent.com>
2026-02-18 18:12:34 +0300

    solution to q1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles 
+of top 10 hot postsfor a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+
+    headers = {
+        "User-Agent": "linux:0-subs:v1.0 (by /Natnael)"
+    }
+
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False
+        )
+
+        if response.status_code != 200:
+            return 0
+
+        data = response.json()
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
+
+    except Exception:
+        return 0

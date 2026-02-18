8c909b218f6519ffb56b72bb52fe4f6f0044019b
NatnaelAyele <n.eticha@alustudent.com>
2026-02-18 18:52:02 +0300

    solution to q1

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,17 +1,32 @@
 #!/usr/bin/python3
-""" return top ten"""
-
+"""Queries the Reddit API and prints the titles of the top 10 hot posts of a subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if not response.ok:
-        print(None)
-        return
-    json_data = response.json()
-    posts = json_data['data']['children']
-    for i in range(min(10, len(posts))):
-        print(posts[i]['data']['title'])
+    """Prints the titles of the first 10 hot posts for a given subreddit."""
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+
+    headers = {
+        "User-Agent": "linux:0-subs:v1.0 (by /Natnael)"
+    }
+
+    try:
+        response = requests.get(
+            url,
+            headers=headers,
+            allow_redirects=False
+        )
+
+        if response.status_code != 200:
+            return 0
+
+        data = response.json()
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+
+
+    except Exception:
+        return 0
